4216152f28e4f762742366baeb00a9112cd526db
mokhtar <mokhtar.rnd@gmail.com>
2019-06-23 00:41:21 +0200

    update login and introduction ui design

--- app/src/main/java/com/twoam/cartello/View/LoginActivity.kt
+++ app/src/main/java/com/twoam/cartello/View/LoginActivity.kt
@@ -2,15 +2,47 @@ package com.twoam.cartello.View
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
 import com.twoam.cartello.R
 
 class LoginActivity : AppCompatActivity() {
 
+    //region Members
+    private lateinit var etEmail: EditText
+    private lateinit var etPassword: EditText
+    private lateinit var tvEmailError: TextView
+    private lateinit var tvPasswordError: TextView
+    private lateinit var tvForgetPassword:TextView
+    private lateinit var tvSkipNow:TextView
+
+
+
+    //endregion
+
+    //region Events
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
     }
 
+    //endregion
+
+    //region Helper Functions
+    //endregion
+
+
+    //region  helper functions
+    private fun validateUserData(email: String, password: String): Boolean {
+        var valid = false
+
+        if (email.isNullOrEmpty()) {
+
+        }
+        return valid
+    }
+
+    //endregion
 
 }

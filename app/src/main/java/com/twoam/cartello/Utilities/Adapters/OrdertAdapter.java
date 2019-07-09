d5f9650b36946196d0e0e2a70e573619699e9220
mokhtar <mokhtar.rnd@gmail.com>
2019-07-09 18:29:06 +0200

    add order desig and its item layout

---
+++ app/src/main/java/com/twoam/cartello/Utilities/Adapters/OrdertAdapter.java
@@ -0,0 +1,103 @@
+package com.twoam.cartello.Utilities.Adapters;
+
+import android.content.Context;
+import android.graphics.Paint;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.twoam.cartello.Model.Order;
+import com.twoam.cartello.R;
+import com.twoam.cartello.Utilities.General.AppController;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Mokhtar on 6/30/2019.
+ */
+
+public class OrdertAdapter
+        extends RecyclerView.Adapter<OrdertAdapter.MyViewHolder> {
+
+    private LayoutInflater inflater;
+    private ArrayList<Order> orderList;
+    private Order order;
+
+
+    public OrdertAdapter(Context ctx, ArrayList<Order> orderList) {
+
+        inflater = LayoutInflater.from(ctx);
+        this.orderList = orderList;
+    }
+
+    @Override
+    public OrdertAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View view = inflater.inflate(R.layout.order_layout, parent, false);
+
+        MyViewHolder holder = new MyViewHolder(view);
+
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(OrdertAdapter.MyViewHolder holder, int position) {
+
+
+        holder.orderID.setText(orderList.get(position).getId());
+        holder.orderDate.setText(orderList.get(position).getCreated_at());
+        holder.totalAmount.setText(orderList.get(position).getPaid_amount() + " " + AppController.getContext().getString(R.string.currency));
+        holder.paymentType.setText(orderList.get(position).getPayment_method());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return orderList.size();
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return super.getItemId(position);
+    }
+
+
+    class MyViewHolder extends RecyclerView.ViewHolder {
+
+        TextView orderID;
+        TextView orderDate;
+        TextView totalAmount;
+        TextView paymentType;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+
+            orderID = itemView.findViewById(R.id.tvOrderIDValue);
+            orderDate = itemView.findViewById(R.id.tvPaymentTypeValue);
+            totalAmount = itemView.findViewById(R.id.tvTotalAmountValue);
+            paymentType = itemView.findViewById(R.id.tvPaymentTypeValue);
+
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    // get position
+                    int pos = getAdapterPosition();
+
+                    // check if item still exists
+                    if (pos != RecyclerView.NO_POSITION) {
+                        order = orderList.get(pos);
+                        Toast.makeText(v.getContext(), "You clicked " + order.getNotes(), Toast.LENGTH_SHORT).show();
+
+                    }
+                }
+            });
+        }
+
+    }
+}

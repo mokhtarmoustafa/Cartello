556a52a4a4a3037c24c2ea57093112ebcf9f1112
mokhtar <mokhtar.rnd@gmail.com>
2019-07-23 20:34:24 +0200

    add product details design

---
+++ app/src/main/java/com/twoam/cartello/Utilities/General/MedicalBottomSheetDialog.kt
@@ -0,0 +1,151 @@
+package com.twoam.cartello.Utilities.General
+
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
+import android.os.Bundle
+import android.provider.MediaStore
+import android.support.design.widget.BottomSheetDialogFragment
+import android.view.*
+import android.widget.*
+import com.twoam.cartello.R
+import com.twoam.cartello.Utilities.DB.PreferenceController
+import com.twoam.cartello.View.*
+import kotlinx.android.synthetic.main.bottom_sheet_close.*
+
+
+class MedicalBottomSheetDialog : BottomSheetDialogFragment(), IBottomSheetCallback, View.OnClickListener {
+
+
+    //region Members
+    internal var view: ViewGroup? = null
+    lateinit var layout: RelativeLayout
+    private var listener: IBottomSheetCallback? = null
+    private var action: Int = 0
+    private lateinit var lLCapture: LinearLayout
+    private lateinit var lLGallery: LinearLayout
+    private lateinit var ivClose: ImageView
+    val REQUEST_IMAGE_CAPTURE = 1
+    val REQUEST_IMAGE_GALLERY = 2
+
+    var Action: Int //0 Camera 1 Gallery
+        get() {
+            return action
+        }
+        set(action) {
+            this.action = action
+        }
+
+    //endregion
+
+    //region Events
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        view = inflater.inflate(R.layout.bottom_sheet_medical, container, false) as ViewGroup
+        layout = view!!.findViewById(R.id.rlOptions)
+
+        init()
+        return view
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is IBottomSheetCallback) {
+            listener = context
+        } else {
+            throw ClassCastException(context.toString() + " must implement IBottomSheetCallback.onBottomSheetSelectedItem")
+        }
+    }
+
+    override fun onBottomSheetSelectedItem(index: Int) {
+
+    }
+
+    override fun onBottomSheetClosed(isClosed: Boolean) {
+        dialog.dismiss()
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.ivClose -> {
+                this.dismiss()
+            }
+
+            R.id.lLCapture -> {
+                this.dismiss()
+                openCamera()
+            }
+            R.id.lLGallery -> {
+                this.dismiss()
+//                navigate(1)
+                openGallery()
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK &&
+                data!!.hasExtra("data")) {
+            val bitmap = data.extras.get("data") as Bitmap
+            Toast.makeText(AppController.getContext(), "Image Saved!", Toast.LENGTH_SHORT).show()
+            if (bitmap != null) {
+//
+            }
+
+        }
+        if (requestCode == REQUEST_IMAGE_GALLERY &&
+                resultCode == Activity.RESULT_OK) {
+            // mImageBitmap reduce image quality -50% and save it in directory
+            //mImageBitmap convert to thumbnail and write it to file and update grid
+            val contentURI = data!!.data ?: return
+            Toast.makeText(AppController.getContext(), "Image Saved!", Toast.LENGTH_SHORT).show()
+
+        }
+    }
+
+    private fun openGallery() {
+        val galleryIntent = Intent(Intent.ACTION_PICK,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+
+        startActivityForResult(galleryIntent, REQUEST_IMAGE_GALLERY)
+    }
+
+    private fun openCamera() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            takePictureIntent.resolveActivity(AppController.getContext().packageManager)?.also {
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+            }
+        }
+    }
+    //endregion
+
+    //region Helper Functions
+    fun init() {
+
+        lLCapture = layout.findViewById(R.id.lLCapture)
+        lLGallery = layout.findViewById(R.id.lLGallery)
+
+        ivClose = layout.findViewById(R.id.ivClose)
+
+        ivClose.setOnClickListener(this)
+        lLCapture.setOnClickListener(this)
+        lLGallery.setOnClickListener(this)
+
+
+    }
+
+    fun navigate(index: Int) {
+        listener?.onBottomSheetSelectedItem(index)
+    }
+
+    private fun logOut() {
+        PreferenceController.getInstance(AppController.getContext()).clear(AppConstants.IS_LOGIN)
+        PreferenceController.getInstance(AppController.getContext()).clear(AppConstants.USER_DATA)
+        context?.startActivity(Intent(context, LoginActivity::class.java))
+        activity?.onBackPressed()
+    }
+    //endregion
+
+}
--- app/src/main/java/com/twoam/cartello/View/MedicalPrescriptionsFragment.kt
+++ app/src/main/java/com/twoam/cartello/View/MedicalPrescriptionsFragment.kt
@@ -1,11 +1,13 @@
 package com.twoam.cartello.View
 
 
+import android.app.FragmentManager
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.Toast
 import com.twoam.Networking.INetworkCallBack
 import com.twoam.Networking.NetworkManager
@@ -18,15 +20,20 @@ import com.twoam.cartello.Utilities.Adapters.MedicalAdapter
 import com.twoam.cartello.Utilities.Base.BaseFragment
 import com.twoam.cartello.Utilities.General.AppConstants
 import com.twoam.cartello.Utilities.General.AppController
+import com.twoam.cartello.Utilities.General.IBottomSheetCallback
+import com.twoam.cartello.Utilities.General.MedicalBottomSheetDialog
 import kotlinx.android.synthetic.main.fragment_medical_prescriptions.*
 
 
-class MedicalPrescriptionsFragment : BaseFragment() {
+class MedicalPrescriptionsFragment : BaseFragment(),IBottomSheetCallback {
 
 
     //region MEMBERS
     private lateinit var currentView: View
     private lateinit var medicalList: ArrayList<MedicalPrescriptions>
+    private var bottomSheet = MedicalBottomSheetDialog()
+    private lateinit var btnAddMedical: Button
+
 
     //endregion
     //region EVENTS
@@ -35,18 +42,37 @@ class MedicalPrescriptionsFragment : BaseFragment() {
         // Inflate the layout for this fragment
         currentView = inflater.inflate(R.layout.fragment_medical_prescriptions, container, false)
 
+        init()
         getAllMedical()
         return currentView
     }
+
+    override fun onBottomSheetClosed(isClosed: Boolean) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBottomSheetSelectedItem(index: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+
     //endregion
     //region HELPER FUNCTIONS
+    private fun init() {
+        btnAddMedical = currentView.findViewById(R.id.btnAddMedical)
+
+        btnAddMedical.setOnClickListener(View.OnClickListener {
+            bottomSheet.Action = 1
+            bottomSheet.show(fragmentManager, "Custom Bottom Sheet")
+        })
+    }
 
     private fun getAllMedical(): ArrayList<MedicalPrescriptions> {
         if (NetworkManager().isNetworkAvailable(AppController.getContext())) {
             var token = AppConstants.BEARER + AppConstants.CurrentLoginUser.token
             var request = NetworkManager().create(ApiServices::class.java)
             var endPoint = request.getAllMedicalPrescriptions(token)
-            NetworkManager().request(endPoint, object:INetworkCallBack<ApiResponse<ArrayList<MedicalPrescriptions>>>{
+            NetworkManager().request(endPoint, object : INetworkCallBack<ApiResponse<ArrayList<MedicalPrescriptions>>> {
                 override fun onFailed(error: String) {
                     hideDialogue()
                     showAlertDialouge(error)
@@ -71,14 +97,17 @@ class MedicalPrescriptionsFragment : BaseFragment() {
 
     private fun prepareMedicalData(medicalList: ArrayList<MedicalPrescriptions>) {
 
-       var  adapter=MedicalAdapter(AppController.getContext(),medicalList)
-        rvMedical.adapter=adapter
+        var adapter = MedicalAdapter(AppController.getContext(), medicalList)
+        rvMedical.adapter = adapter
         rvMedical.layoutManager = LinearLayoutManager(AppController.getContext(), LinearLayoutManager.VERTICAL, false)
     }
 
     fun addMedical(token: String, name: String, note: String, image: String) {
-        //todo
+
     }
+
+    fun loadMedicalDetails()
+    {}
     //endregion
 
 
